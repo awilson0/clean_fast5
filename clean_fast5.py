96c4ba4b3ad35b23901ddddf2cf9eda0e5604d22
Andrew Wilson <andrew.wilson@pnnl.gov>
2023-05-02 14:00:57 -0700

    adds argument parser

--- clean_fast5.py
+++ clean_fast5.py
@@ -4,3 +4,11 @@ import argparse
 from subprocess import os, PIPE, run
 from ont_fast5_api.fast5_interface import get_fast5_file
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="remove skipped reads from fast5 files")
+    parser.add_argument("in", type="str", help="path to fast5 files")
+    parser.add_argument("out", type="str", help="path to save cleaned fast5 files")
+    return parser.parse_args()
+
+
